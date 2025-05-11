6a5895cb6292c58bad4bdf856091cca8d1c33304
HottyMcShotty <samueliscool2015@gmail.com>
2025-05-10 20:23:40 -0500

    Initial commit

---
+++ src/main/java/net/thetrog/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.thetrog.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
